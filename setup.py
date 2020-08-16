bcd2b7e539214262bcfab5054eafb28e08384c37
Asmita Mittal <asmi@eduroam-ipv4-1148.triple-a.uni-kl.de>
2020-08-16 17:19:59 +0200

    Ïnitial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Applying data science tools to analyze the spread of COVID-19 virus',
+    author='Asmita',
+    license='MIT',
+)
